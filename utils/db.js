0cf2d1f600c4d77f1d6e5588e29858dcc497ce6d
Krishna Singh <krishna00cs@gmail.com>
2024-08-19 12:30:56 +0530

    final-commit

---
+++ utils/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+// console.log(process.env.MONGO_URI);
+const url = process.env.MONGO_URI;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(url, {});
+        console.log('mongodb connected successfully');
+    } catch (error) {
+        console.error(error);
+        console.log("cant connect");
+    }
+}
+export default connectDB;
