a69659932f4adb62e3e34703638b9f26d8c29877
Krishna Singh <krishna00cs@gmail.com>
2024-09-02 21:36:46 +0530

    last-commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',  // Only the origin
+    origin: 'https://job-portal-krishna.netlify.app',  // Only the origin
     credentials: true
 }
 
